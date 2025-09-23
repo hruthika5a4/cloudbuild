8751337145b9e405f4aef38ef8836a3aad2a9923
hruthika5a4 <131320755+hruthika5a4@users.noreply.github.com>
2025-09-23 20:13:47 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-set -e
 
-# Pull the image from Artifact Registry
-docker pull $DOCKER_IMAGE
-
-# Stop any existing container
-docker rm -f web-app || true
-
-# Run the new container
-docker run -d --name web-app -p 80:8080 $DOCKER_IMAGE
